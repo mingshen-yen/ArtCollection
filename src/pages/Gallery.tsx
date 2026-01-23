ebc5f7d3d8b41dbcde3bd798d57a9b009821b469
mingshen-yen <minghsuanyen@gmail.com>
2026-01-23 18:07:38 +0100

    styling

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router";
-import { Plus, MoveLeft } from "lucide-react";
+import { Plus } from "lucide-react";
 import { useEffect, useState } from "react";
 import type { Art, SavedArt } from "../types";
 import CollectedCard from "../components/ui/CollectedCard";
