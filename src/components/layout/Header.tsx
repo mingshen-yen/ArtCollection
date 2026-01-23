953621dc3a81f5358fe04b6eae624bcb9888ce41
mingshen-yen <minghsuanyen@gmail.com>
2026-01-23 15:53:03 +0100

    fixed header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router";
 
 export default function Header() {
   return (
-    <div className="bg-slate-300 shadow-sm">
+    <div className="fixed top-0 left-0 right-0 z-50 bg-slate-300 shadow-sm">
       <div className="flex items-center justify-between px-4 md:px-8 py-4 max-w-7xl mx-auto">
         {/* Logo */}
         <div className="flex items-center gap-2">
